3eb1d2cb7633eefbbee151e17911f99aab3ad98b
Solange Harmonie PICARD <solange.picard@cryptolab.net>
2019-09-08 16:42:28 +0200

    Ajout script btn scroll to top

--- script.js
+++ script.js
@@ -12,3 +12,18 @@ $(document).ready(function(){
     }
   });
 });
+
+/* SCROLL TO TOP */
+var mybutton = document.getElementById("btnTop");
+window.onscroll = function() {scrollFunction()};
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
